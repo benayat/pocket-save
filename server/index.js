3def3c3ed2046aabbafa55222fb056ee7dd7377d
Benaya <benaya7@gmail.com>
2021-05-20 06:18:35 +0300

    ascasca

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(router);
 
 // # use alternate localhost and the port Heroku assigns to $PORT
-const host = "0.0.0.0";
+// const host = "0.0.0.0";
 const port = process.env.PORT || 5000;
-app.listen(port, host, () => {
+app.listen(port, () => {
   console.log("server is up on 5000");
 });
