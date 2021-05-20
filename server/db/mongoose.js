25a1c432583c3239cc3ccd2344a67166e422d2c3
Benaya <benaya7@gmail.com>
2021-05-20 05:39:41 +0300

    connecting to mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/pocketSave", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://benayat:fmWAK3TLrJwHxr8@cluster0.ptwdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
 
 // "mongodb+srv://benayat:fmWAK3TLrJwHxr8@bank-api-db.sncs8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
