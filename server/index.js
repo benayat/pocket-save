d3e67d72b7f40baa9b7251de01d7b7163bce8840
Benaya <benaya7@gmail.com>
2021-05-20 04:56:22 +0300

    tdrdx

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(router);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+// # use alternate localhost and the port Heroku assigns to $PORT
+const host = "0.0.0.0";
+const port = process.env.PORT || 5000;
+app.listen(port, host, () => {
   console.log("server is up on 5000");
 });
